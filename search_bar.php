6db67ae5b6c44273c44af975be76543b3860436e
GargSidhartha <gsidhartha53@gmail.com>
2024-03-20 12:46:01 +0530

    update searchbar

--- search_bar.php
+++ search_bar.php
@@ -25,8 +25,7 @@ include ('./functions/common_function.php'); // Common functions file
 
 <body>
 
-    <!-- responsive navbar - container fluid is a bootstrap class which takes complete 100% width -->
-    <div class="container-fluid p-0">
+<div class="container-fluid p-0">
         <!-- first child -->
         <nav class="navbar navbar-expand-lg bg-info">
             <div class="container-fluid">
@@ -41,30 +40,73 @@ include ('./functions/common_function.php'); // Common functions file
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                         </li>
+
                         <li class="nav-item">
                             <a class="nav-link" href="index.php">Products</a>
                         </li>
+
                         <li class="nav-item">
                             <a class="nav-link" href="#">Reviews</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><i class="fa fa-shopping-cart"></i><sup> <?php cart_item();?></sup></a>
-                        </li>
-                        <li class = "total_price">
-                            <a href="#" class="nav-link">Total Price: Rs. <?php total_cart();?></a>
+
+                        <li class = "nav-item">
+                        <div class="nav-link"><i id="shopping-cart-icon" class="fa fa-shopping-cart" data-bs-toggle="dropdown"></i><sup> <?php cart_item();?></sup>
+                            <div class="dropdown-menu" id="cart-dropdown">
+                                <div class="cart-dropdown-header">
+                                    <h6>My Cart</h6>
+                                </div>
+                                <div class="cart-dropdown-body" style="max-height: 50vh; overflow-y: auto; width: 40vh;">
+                                    <?php show_cart();?>
+                                </div>
+                                <div class="cart-dropdown-footer">
+                                    <a href="cart.php" class="btn btn-primary">Go to Cart</a>
+                                    <?php
+                                    $totalPrice = total_cart(); // Get the total cart price
+                                    $walletBalance = wallet(); // Get the wallet balance
+
+                                    if ($totalPrice <= $walletBalance) {
+                                        echo '<a href="checkout.php" class="btn btn-success" >Checkout</a>'; // Allow checkout if wallet has sufficient funds
+                                    } else {
+                                        echo '<p>Insufficient funds in wallet, cannot checkout!</p>'; // Display message for insufficient funds
+                                    }
+                                    ?>
+
+                                    <strong>Rs. <?php echo total_cart();?></strong>
+                                </div>
+                            </div>
+                        </div>
                         </li>
 
+                        <li class = "wallet">
+                            <!-- use a wallet symbol -->
+                            <a href="#" class="nav-link"><i class = "fa fa-wallet"></i> Wallet: Rs. <?php echo wallet(); ?></a>
+                        </li>
+                        
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="cart.php"><i class="fa fa-shopping-cart"></i><sup> <?php cart_item();?></sup></a>
+                        </li> -->
+                        <li class = "total_price">
+                            <a href="#" class="nav-link">Total Price: Rs. <?php echo total_cart();?></a>
+                        </li>
 
                     </ul>
-                    <form class="d-flex" role="search" action="" method="get">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"
-                            name="search_bar">
+                    
+                    <form class="d-flex" role="search" action="search_bar.php" method="get">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_bar">
                         <!-- <button class="btn btn-outline-success" type="submit">Search</button> -->
                         <input type="submit" value="Search" class="btn btn-outline-success" name="search_data">
                     </form>
                 </div>
             </div>
         </nav>
+
+        <!-- calling Cart function -->
+        
+        <?php
+        Cart();
+        remove_cart();
+        updateCart();
+        ?>
 
 
         <!-- second child -->
